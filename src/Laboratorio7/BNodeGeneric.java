e06b7d2adeabe2e03581f757fc75360607995922
cvenero <cvenero@unsa.edu.pe>
2022-08-11 01:43:58 -0500

    Se agrega el proyecto

---
+++ src/Laboratorio7/BNodeGeneric.java
@@ -0,0 +1,28 @@
+package Laboratorio7;
+
+import java.util.Vector;
+
+public class BNodeGeneric<T> {
+	
+	Vector<T> keys;
+	int MinDeg; // Minimum degree of B-tree node
+	Vector<BNodeGeneric<T>> children;
+	int num; // Number of keys of node
+    boolean isLeaf; // True when leaf node
+   
+    
+    
+	
+ // Constructor
+    BNodeGeneric(int deg,boolean isLeaf){
+    	
+        this.MinDeg = deg;
+        this.isLeaf = isLeaf;
+        this.keys = new Vector<T>(2*this.MinDeg-1); // Node has 2*MinDeg-1 keys at most
+        this.children = new Vector<BNodeGeneric<T>>(2*this.MinDeg);
+        //this.children = new BTreeNode[2*this.MinDeg];
+        this.num = 0;
+    }
+	
+	
+}
